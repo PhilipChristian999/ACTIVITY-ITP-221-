9ab0c4a6c4848cfb7eff9a21f4a3db2e68a1263d
PHILIP CHRISTIAN GEROCA <philipcalvara098@gmail.com>
2025-02-14 15:27:23 +0800

    task2.py.py

--- myPYTHON/task2.py.py
+++ myPYTHON/task2.py.py
@@ -7,12 +7,10 @@ class Student:
     def introduce(self):
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I am studying {self.course}.")
 
-# Create three student objects
 student1 = Student("Hayabusa", 23, "Aerospace Engineer")
 student2 = Student("Lancelott", 22, "Architecture")
 student3 = Student("Ling", 21, "Psychology")
 
-# Call their introduce() method
 student1.introduce()
 student2.introduce()
 student3.introduce()
