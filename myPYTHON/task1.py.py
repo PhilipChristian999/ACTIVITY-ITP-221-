d21638d2800f73c8364805d7f644c485ff41bf13
PHILIP CHRISTIAN GEROCA <philipcalvara098@gmail.com>
2025-02-14 15:26:58 +0800

    task1.py.py

--- myPYTHON/task1.py.py
+++ myPYTHON/task1.py.py
@@ -9,11 +9,8 @@ class Car:
         print(f"Car Model: {self.model}")
         print(f"Car Year: {self.year}")
 
-# Create two car objects
 car1 = Car("Lamborghini", "Huracan", 2014)
 car2 = Car("Koenigsegg ", "Agera", 2010)
-
-# Display their details
 car1.display_info()
-print()  # Add a newline for better readability
+print()
 car2.display_info()
